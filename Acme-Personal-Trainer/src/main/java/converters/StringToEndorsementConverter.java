4c3d4c3d3916d87432bea6c117d0beeae386c46f
marjimveg1 <maria-2696@hotmail.com>
2019-05-17 18:04:06 +0200

    covnerters, i18n-l10n, tiles

---
+++ Acme-Personal-Trainer/src/main/java/converters/StringToEndorsementConverter.java
@@ -0,0 +1,48 @@
+/*
+ * StringToBoxConverter.java
+ * 
+ * Copyright (C) 2017 Universidad de Sevilla
+ * 
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
+ * http://www.tdg-seville.info/License.html
+ */
+
+package converters;
+
+import javax.swing.Box;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class StringToEndorsementConverter implements Converter<String, Endorsement> {
+
+	@Autowired
+	EndorsementRepository	endorsementRepository;
+
+
+	@Override
+	public Endorsement convert(final String text) {
+		Box result;
+		int id;
+
+		try {
+			if (StringUtils.isEmpty(text))
+				result = null;
+			else {
+				id = Integer.valueOf(text);
+				result = this.endorsementRepository.findOne(id);
+			}
+		} catch (final Throwable oops) {
+			throw new IllegalArgumentException(oops);
+		}
+
+		return result;
+	}
+
+}
