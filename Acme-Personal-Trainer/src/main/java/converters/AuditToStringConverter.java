d275c41f2c04234dcae7f39f52b03bcfadf91d3c
marjimveg1 <maria-2696@hotmail.com>
2019-05-17 18:05:26 +0200

    Revert "Revert "covnerters, i18n-l10n, tiles""
    This reverts commit 4c928f8b0d540b986f1b56602815afd838f75c6b.

---
+++ Acme-Personal-Trainer/src/main/java/converters/AuditToStringConverter.java
@@ -0,0 +1,33 @@
+/*
+ * AuditToStringConverter.java
+ * 
+ * Copyright (C) 2017 Universidad de Sevilla
+ * 
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
+ * http://www.tdg-seville.info/License.html
+ */
+
+package converters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class AuditToStringConverter implements Converter<Audit, String> {
+
+	@Override
+	public String convert(final Audit audit) {
+		String result;
+
+		if (audit == null)
+			result = null;
+		else
+			result = String.valueOf(audit.getId());
+
+		return result;
+	}
+
+}
